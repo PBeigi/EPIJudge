1e3f3c0bc1731fe5318a919ab4c3a6f753dc1180
Pouya Beigi <pouy4bg@gmail.com>
2021-09-16 23:28:05 -0500

    array increment

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -5,12 +5,12 @@ from test_framework import generic_test
 
 def plus_one(A: List[int]) -> List[int]:
     A[-1] += 1
-    i = len(A) - 1
-    while i >= 1:
-        if A[i] == 10:
-            A[i] = 0
-            A[i - 1] += 1
-        i-=1
+    for i in reversed(range(1, len(A))):
+        if A[i] < 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+        i -=1
     if A[0] == 10:
         A[0] = 1
         A.append(0)
