1ca78fcc57bc2d8747ad396b8c9e345535534ccb
Pouya Beigi <pouy4bg@gmail.com>
2021-09-18 19:18:55 -0500

    next permulation

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -4,7 +4,16 @@ from test_framework import generic_test
 
 
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
+    for i in reversed(range(len(perm) - 1)):
+        if perm[i] < perm[i+1]:
+            smallest = float('inf')
+            smallest_idx = -1
+            for j in range(i+1, len(perm)):
+                if (perm[j] > perm[i]) and (perm[j] < smallest):
+                    smallest = perm[j]
+                    smallest_idx = j
+            perm[i], perm[smallest_idx] = perm[smallest_idx], perm[i]
+            return perm[:i+1] + sorted(perm[i+1:])
     return []
 
 
