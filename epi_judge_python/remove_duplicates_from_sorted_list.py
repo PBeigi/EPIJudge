edce6643193e03f95f31f8e2e003e42168d2f159
Pouya Beigi <pouy4bg@gmail.com>
2021-09-30 23:52:15 -0500

    remove el from sorted list

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -5,14 +5,42 @@ from test_framework import generic_test
 
 
 def remove_duplicates(L: ListNode) -> Optional[ListNode]:
+
+    # cur = L
+    # while cur:
+    #     while cur.next and cur.next.data == cur.data:
+    #         cur.next = cur.next.next
+    #     cur = cur.next
+    # return L
+    # cur = L
+    #
+    # while cur:
+    #     next_distinct = cur.next
+    #     while next_distinct and next_distinct.data == cur.data:
+    #         next_distinct = next_distinct.next
+    #     cur.next = next_distinct
+    #     cur = next_distinct
+    # return L
+
     it = cur = L
     while cur and it.next:
         if it.next.data == cur.data:
             it.next = it.next.next
         else:
             it = it.next
-            cur = cur.next
+            cur = it
     return L
+
+    while cur and it.next:
+        if it.next.data == cur.data:
+            it = it.next
+        else:
+            cur.next = it.next
+            cur = it.next
+            it = cur
+    return L
+
+
 
 
 
