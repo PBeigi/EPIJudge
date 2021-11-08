56e7f0c21fe13185b5ed03770e600cab45220e71
Pouya Beigi <pouy4bg@gmail.com>
2021-11-08 17:31:56 -0600

    updates

--- epi_judge_python/valid_ip_addresses.py
+++ epi_judge_python/valid_ip_addresses.py
@@ -2,23 +2,31 @@ from typing import List
 
 from test_framework import generic_test
 
+def dfs2(s, part_count, path, res):
+    if part_count > 4:
+        return
+    if part_count==4 and len(s)==0:
+        res.append(path[:-1])
+    for i in range(1, min(4, len(s)+1)):
+        part = s[:i]
+        if len(part) == 1 or (part[0] != '0' and int(part) <= 255):
+            dfs2(s[i:], part_count + 1, path + part + ".", res)
+
+def dfs(s, cur_idx, part_count, path, res):
+    if part_count > 4:
+        return
+    if part_count==4 and cur_idx >= len(s):
+        res.append(path[:-1])
+    for i in range(cur_idx+1,min(cur_idx+4, len(s)+1)):
+        part = s[cur_idx:i]
+        if len(part)==1 or (part[0]!='0' and int(part)<=255):
+            #print(f"cur: {cur_idx}, part_count: {part_count}, section: {part}, path: {path}")
+            dfs(s, i, part_count+1, path+part+".", res)
+
 
 def get_valid_ip_address(s: str) -> List[str]:
     results = []
-    def is_valid(s):
-        return len(s)==1 or (s[0]!='0' and int(s) <= 255)
-
-    for i in range(1, min(4, len(s))):
-        part_1 = s[:i]
-        if is_valid(part_1):
-            for j in range(i+1, min(i+4, len(s))):
-                part_2 = s[i:j]
-                if is_valid(part_2):
-                    for k in range(j+1, min(j+4, len(s))):
-                        part_3 = s[j:k]
-                        part_4 = s[k:]
-                        if is_valid(part_3) and is_valid(part_4):
-                            results.append(f"{part_1}.{part_2}.{part_3}.{part_4}")
+    dfs2(s,0,"", results)
     return results
 
 
