5be9639c237ff52e904306e9be42283b204fd892
Pouya Beigi <pouy4bg@gmail.com>
2021-12-15 19:13:57 -0600

    updated

--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -4,11 +4,25 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
+import collections
+
+Status = collections.namedtuple('Status', ('node', 'next_index'))
+
+def helper(preorder, index):
+    cur = preorder[index]
+    if cur is None:
+        return Status(node=None, next_index=index+1)
+    cur_node = BinaryTreeNode(cur)
+    status = helper(preorder, index+1)
+    cur_node.left = status.node
+    status = helper(preorder, status.next_index)
+    cur_node.right = status.node
+    return Status(node=cur_node, next_index=status.next_index)
 
 
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    status = helper(preorder, 0)
+    return status.node
 
 
 @enable_executor_hook
